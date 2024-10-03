c916585fcf6a33275ed1a5ab3bdcd8ae1dc2d240
Moana <sabiu7108@gmail.com>
2024-10-04 01:05:43 +0900

    Making it running

--- src/components/AffiliateModal/CampaignsTab/CampaignsTab.tsx
+++ src/components/AffiliateModal/CampaignsTab/CampaignsTab.tsx
@@ -1,7 +1,7 @@
 import Button from '../../../shared/Button';
 import DividerDiv from '../../DividerDiv';
 import CampaignInfo from '../CampaignInfo';
-import CampaignsTable from '../CampaignsTable';
+import CampaignsTable from '../CampaignsTable/CampaignsTable';
 import CreateCampaignForm from '../CreateCampaignForm';
 import InviteFriendGuide from '../InviteFriendGuide';
 
@@ -40,8 +40,7 @@ const CampaignsTab = () => {
       <DividerDiv className="!bg-[#1c2127]" />
       <CampaignsTable headers={headers} list={list} />
       <Button
-        className="bg-[#f8bf60] text-black rounded-[6px]
-                h-[40px]"
+        className="h-[40px] rounded-[6px] bg-[#f8bf60] text-black"
       >
         STAKE COMMISSION
       </Button>
