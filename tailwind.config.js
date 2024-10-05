fe15f2e21214759a32f7dbb1209dcb2551b46d85
Moana <sabiu7108@gmail.com>
2024-10-06 04:34:53 +0900

    Fixing hiding of Second Section Issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       center: true
     },
     extend: {
+      gridTemplateColumns: {
+        15: 'repeat(15, minmax(0, 1fr))'
+      },
       fontFamily: {
         indieflower: ['IndieFlower', 'san-serif'],
         aldrich: ['Aldrich', 'sans-serif'],
